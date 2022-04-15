1b9e0e2dfcd5ff652320008e5d85044db4b9fe13
Robert Slocumbe <rslocumbe93@gmail.com>
2022-04-15 18:08:32 -0400

    made init()

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# readme_generator
+{
+	"username": "fdsafdsa",
+	"email": "fdsafdsaf",
+	"project_name": "dsafdsaf",
+	"description": "dsafdsa",
+	"license": "MIT",
+	"dependencies_cmd": "fdsaf",
+	"test_cmd": "dsafds",
+	"tips": "afdsa",
+	"how_to_contribute": "fdsa"
+}
--- index.js
+++ index.js
@@ -1,15 +1,82 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const markdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is your project's name?",
+    "Please write a short description of your project?",
+    "What kind of license should your project have?", // options
+    "What command should be run to install dependencies",
+    "What command should be run to run tests?",
+    "What does the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: questions[0],
+                name: "username"
+            },
+            {
+                type: "input",
+                message: questions[1],
+                name: "email"
+            },
+            {
+                type: "input",
+                message: questions[2],
+                name: "project_name"
+            },
+            {
+                type: "input",
+                message: questions[3],
+                name: "description"
+            },
+            {
+                type: "list",
+                message: questions[4],
+                name: "license",
+                choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+            },
+            {
+                type: "input",
+                message: questions[5],
+                name: "dependencies_cmd"
+            },
+            {
+                type: "input",
+                message: questions[6],
+                name: "test_cmd"
+            },
+            {
+                type: "input",
+                message: questions[7],
+                name: "tips"
+            },
+            {
+                type: "input",
+                message: questions[8],
+                name: "how_to_contribute"
+            },
+        ])
+        .then((data) => {
+            console.log(data);
+            fs.writeFile("README.md", data, (err) =>
+            err ? console.log(err) : console.log("making README...")
+            );
+        });
+}
 
 // Function call to initialize app
 init();
